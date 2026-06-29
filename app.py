e100e9ec9e606bbe7287f50b75297a50e2d4f03a
Nishtha Shukla <your-email@example.com>
2026-06-29 22:35:31 +0530

    Day 2 - HTML page connected with Flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "AI Interview Coach is live!"
+    return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
