d55cb7b08fd2b5230c3f75130b3371bf70b10d71
Nishtha Shukla <your-email@example.com>
2026-06-30 10:36:06 +0530

    Day 4 - Profile form with SQLite database connected

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 app = Flask(__name__)
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS profile (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            degree TEXT,
+            college TEXT,
+            year TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/save", methods=["POST"])
+def save_profile():
+    name = request.form["name"]
+    degree = request.form["degree"]
+    college = request.form["college"]
+    year = request.form["year"]
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO profile (name, degree, college, year) VALUES (?, ?, ?, ?)",
+                   (name, degree, college, year))
+    conn.commit()
+    conn.close()
+
+    return "Profile saved!"
 if __name__ == "__main__":
     app.run(debug=True)
